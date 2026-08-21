96fdb845b0afa5b3255a66bf4b03373dd8886ff9
mooreprintpages-hub <mooreprintpages@gmail.com>
2026-08-21 10:40:25 -0600

    feat: enable customers and orders modules

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import OperationalApp from './OperationalApp'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <OperationalApp />
   </StrictMode>,
 )
